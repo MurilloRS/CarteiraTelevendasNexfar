8f5708afae0c060f65832c14c5f996a00762c270
Murillo Rodrigues <murillorodrigues1409@gmail.com>
2024-05-07 15:54:07 -0300

    final

--- main.py
+++ main.py
@@ -1,63 +1,58 @@
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, scrolledtext
 
 import openpyxl
 import pandas as pd
 import pyodbc
 
-from sql import Telev, carteiraTelev, cliente
+from sql import carteira, cliente_query, delete, insert, query, vendedor_query
 
-# Construir a string de conexão
-connection_string = (
-    r'DRIVER={SQL Server};'
-    r'SERVER=192.168.1.22;'
-    r'DATABASE=TESTE;'
-    r'UID=sa;'
-    r'PWD=Moitgt2526;'
-)
-
+# # Construir a string de conexão
 # connection_string = (
 #     r'DRIVER={SQL Server};'
-#     r'SERVER=192.168.2.10;'
-#     r'DATABASE=MOINHO;'
+#     r'SERVER=192.168.1.22;'
+#     r'DATABASE=TESTE;'
 #     r'UID=sa;'
-#     r'PWD=moitgt2526;'
+#     r'PWD=Moitgt2526;'
 # )
 
-# Consulta SQL para verificar a existência da combinação (cd_clien, cd_vend)
-query = "SELECT * FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
-insert = "INSERT INTO dbo.clientelev (cd_clien,cd_vend) VALUES (?, ?);"
-delete = "DELETE FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
-cliente_query = "SELECT * FROM dbo.cliente WHERE cd_clien = ?"
-vendedor_query = "SELECT * FROM dbo.vendedor where cd_vend = ?"
-carteira = """select
-                    c.cd_clien,
-                    c.cd_vend,
-                    '' as '           ',
-                    cl.cd_clien codCliente,
-                    cl.nome cliente,
-                    ec.estado,
-                    vd.cd_vend codVendedor,
-                    vd.nome vendedor,
-                    eq.descricao equipe
-                from
-                    clientelev c
-                    left join cliente cl on cl.cd_clien = c.cd_clien
-                    left join vendedor vd on vd.cd_vend = c.cd_vend
-                    left join end_cli ec on ec.cd_clien = cl.cd_clien and ec.tp_end = 'FA'
-                    left join equipe eq on eq.EquipeId = vd.EquipeID"""
+connection_string = (
+    r'DRIVER={SQL Server};'
+    r'SERVER=192.168.2.10;'
+    r'DATABASE=MOINHO;'
+    r'UID=sa;'
+    r'PWD=moitgt2526;'
+)
 
+# # Consulta SQL para verificar a existência da combinação (cd_clien, cd_vend)
+# query = "SELECT * FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
+# insert = "INSERT INTO dbo.clientelev (cd_clien,cd_vend) VALUES (?, ?);"
+# delete = "DELETE FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
+# cliente_query = "SELECT * FROM dbo.cliente WHERE cd_clien = ?"
+# vendedor_query = "SELECT * FROM dbo.vendedor where cd_vend = ?"
+# carteira = """select
+#                     c.cd_clien,
+#                     c.cd_vend,
+#                     '' as '           ',
+#                     cl.cd_clien codCliente,
+#                     cl.nome cliente,
+#                     ec.estado,
+#                     vd.cd_vend codVendedor,
+#                     vd.nome vendedor,
+#                     eq.descricao equipe
+#                 from
+#                     clientelev c
+#                     left join cliente cl on cl.cd_clien = c.cd_clien
+#                     left join vendedor vd on vd.cd_vend = c.cd_vend
+#                     left join end_cli ec on ec.cd_clien = cl.cd_clien and ec.tp_end = 'FA'
+#                     left join equipe eq on eq.EquipeId = vd.EquipeID"""
 
 # Função para conectar ao banco de dados e verificar a existência da combinação (cd_clien, cd_vend)
 def verificar_combinacao(cd_clien, cd_vend, aba):
     # Converter os valores para string
     cd_clien_str = str(cd_clien)
     cd_vend_str = str(cd_vend)
-
-    # Imprimir os valores antes da execução da consulta SQL
-    # print("cd_clien:", cd_clien_str)
-    # print("cd_vend:", cd_vend_str)
 
     # Conectar ao banco de dados
     conn = pyodbc.connect(connection_string)
@@ -101,8 +96,6 @@ def verificar_combinacao(cd_clien, cd_vend, aba):
         mensagem = f"\n(cd_clien = {cd_clien_str}, cd_vend = {cd_vend_str}) - não existe."
         text_box.insert(tk.END, mensagem)
         if aba == "Incluir":
-            # mensagem_insert = "Executando INSERT..."
-            # text_box.insert(tk.END, mensagem_insert)
             cursor.execute(insert, (cd_clien_str, cd_vend_str))
             conn.commit()
             mensagem = f"\n - INCLUÍDO  COM SUCESSO."
@@ -134,36 +127,42 @@ def selecionar_planilha():
     # Abrir uma janela para selecionar um arquivo
     filename = filedialog.askopenfilename(filetypes=[("Planilha Excel", "*.xlsx")])
     if filename:
-        # Exibir o nome do arquivo lido
-        nome_arquivo = os.path.basename(filename)
-        text_box.insert(tk.END, f"Arquivo: {nome_arquivo}")
+        try:
+            # Exibir o nome do arquivo lido
+            nome_arquivo = os.path.basename(filename)
+            text_box.insert(tk.END, f"Arquivo: {nome_arquivo}")
 
-        # Ler a planilha
-        workbook = openpyxl.load_workbook(filename)
-        for sheet_name in workbook.sheetnames:
-            text_box.insert(tk.END, f"\n\nAba: {sheet_name}\n")
-            sheet = workbook[sheet_name]
-            # Variável para pular a primeira linha
-            primeira_linha = True
-            for row in sheet.iter_rows(values_only=True):
-                # Pular a primeira linha
-                if primeira_linha:
-                    primeira_linha = False
-                    continue
-                # Verificar se a linha está vazia
-                if any(cell is not None for cell in row):
-                    # Verificar a existência da combinação (cd_clien, cd_vend)
-                    verificar_combinacao(row[0], row[1], sheet_name)
-        # Fechar a planilha após o uso
-        workbook.close()
+            # Ler a planilha
+            workbook = openpyxl.load_workbook(filename)
+            for sheet_name in workbook.sheetnames:
+                text_box.insert(tk.END, f"\n\nAba: {sheet_name}\n")
+                sheet = workbook[sheet_name]
+                # Verificar se as colunas esperadas estão presentes
+                expected_columns = ("cd_clien", "cd_vend")  # Colunas esperadas
+                header_row = next(sheet.iter_rows(min_row=1, max_row=1, values_only=True), None)
+                if header_row is None or not all(column_name in header_row for column_name in expected_columns):
+                    raise ValueError("A planilha não possui as colunas esperadas.")
+                # Variável para pular a primeira linha
+                primeira_linha = True
+                for row in sheet.iter_rows(min_row=2, values_only=True):  # Começar da segunda linha
+                    # Verificar se a linha está vazia
+                    if any(cell is not None for cell in row):
+                        # Verificar a existência da combinação (cd_clien, cd_vend)
+                        verificar_combinacao(row[0], row[1], sheet_name)
+            # Fechar a planilha após o uso
+            workbook.close()
+        except FileNotFoundError:
+            text_box.insert(tk.END, "Erro: Arquivo não encontrado.")
+        except ValueError as e:
+            text_box.insert(tk.END, f"Erro: {str(e)}")
 
 
 # Criar a janela tkinter
 root = tk.Tk()
 root.title("Selecionar Planilha")
 
-# Componente de texto para exibir mensagens
-text_box = tk.Text(root, height=20, width=100)
+# Componente de texto com barra de rolagem para exibir mensagens
+text_box = scrolledtext.ScrolledText(root, height=20, width=100)
 text_box.pack(padx=20, pady=10, fill=tk.BOTH, expand=True)
 
 # Botão para selecionar a planilha
--- sql.py
+++ sql.py
@@ -1,5 +1,22 @@
-carteiraTelev = "SELECT cd_clien FROM dbo.clientelev where cd_vend = ?"
-
-Telev = "select cd_vend, nome nome_televendas from dbo.vendedor telev where categ = '05'"
-
-cliente = 'select cd_clien,nome_res nome_cliente from dbo.cliente where cd_vend in (?)'
+# Consulta SQL para verificar a existência da combinação (cd_clien, cd_vend)
+query = "SELECT * FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
+insert = "INSERT INTO dbo.clientelev (cd_clien,cd_vend) VALUES (?, ?);"
+delete = "DELETE FROM dbo.clientelev WHERE cd_clien = ? AND cd_vend = ?"
+cliente_query = "SELECT * FROM dbo.cliente WHERE cd_clien = ?"
+vendedor_query = "SELECT * FROM dbo.vendedor where cd_vend = ?"
+carteira = """select
+                    c.cd_clien,
+                    c.cd_vend,
+                    '' as '           ',
+                    cl.cd_clien codCliente,
+                    cl.nome cliente,
+                    ec.estado,
+                    vd.cd_vend codVendedor,
+                    vd.nome vendedor,
+                    eq.descricao equipe
+                from
+                    clientelev c
+                    left join cliente cl on cl.cd_clien = c.cd_clien
+                    left join vendedor vd on vd.cd_vend = c.cd_vend
+                    left join end_cli ec on ec.cd_clien = cl.cd_clien and ec.tp_end = 'FA'
+                    left join equipe eq on eq.EquipeId = vd.EquipeID"""
